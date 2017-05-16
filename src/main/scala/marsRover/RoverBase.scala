0740385ef2dc311c8f47215549eb82faf8840437
Luke <11lroberts@madeleyacademy.com>
2017-05-16 15:22:48 +0100

    Everything but obstacles

--- src/main/scala/marsRover/RoverBase.scala
+++ src/main/scala/marsRover/RoverBase.scala
@@ -4,23 +4,49 @@ import scala.math
   * Created by luke on 16/05/17.
   */
 
+class Position(var x: Int, var y: Int)
 
-object Rover extends App{
-
-}
-class Position(val x: Int, val y: Int)
 class Obstacle(val location: Position)
 
+class Rover(var location: Position = new Position(0,0), var direction: Char = 'N', var instructions: List[Char] = List.empty[Char]){
 
-class Rover(val location: Position, val direction: Char, val instructions: Array[Char]){
+  def getCurrentPosition(): Position = location
 
+  def getCurrentDirection(): Char = direction
+
+  private def setDirection(newDirection: Char): Unit =(direction = newDirection)
+
+  private def setPosition(newPosition: Position): Unit =(location = newPosition)
+
+  private def turn(direction: Char): Unit = setDirection(direction)
+
+  private def step(): Unit = {
+    getCurrentDirection() match{
+      case 'N' => setPosition(new Position(getCurrentPosition().x, getCurrentPosition().y + 1))
+      case 'S' => setPosition(new Position(getCurrentPosition().x, getCurrentPosition().y + -1))
+      case 'E' => setPosition(new Position(getCurrentPosition().x + 1, getCurrentPosition().y))
+      case 'W' => setPosition(new Position(getCurrentPosition().x + -1, getCurrentPosition().y))
+    }
+  }
+
+  def instructRover(instructions: List[Char]): Boolean = {
+    for (i <- instructions.indices) {
+      if ("NESW".contains(instructions(i))) {
+        setDirection(instructions(i))
+      } else if (instructions(i) == 'M') {
+        step()
+      } else {
+        return false
+      }
+    }
+    return true
+  }
 }
 
 class Planet(val minPos: Position, val maxPos: Position){
-  def calculateSize: Int ={
-    val xSize: Int = scala.math.abs(minPos.x) + scala.math.abs(maxPos.x)
-    val ySize: Int = scala.math.abs(minPos.y) + scala.math.abs(maxPos.y)
-    return xSize * ySize
-  }
+
+  def calculateSize: Int = (scala.math.abs(minPos.x) + scala.math.abs(maxPos.x)) * (scala.math.abs(minPos.y) + scala.math.abs(maxPos.y))
+
 }
+
 
--- src/test/scala/marsRover/RoverBaseSpec.scala
+++ src/test/scala/marsRover/RoverBaseSpec.scala
@@ -1,6 +1,7 @@
 package marsRover
 
 import base.UnitSpec
+import com.sun.xml.internal.ws.dump.LoggingDumpTube.Position
 import org.scalatest.{FlatSpec, Matchers}
 /**
   * Created by luke on 16/05/17.
@@ -19,8 +20,58 @@ If a given sequence of commands encounters an obstacle, the rover moves up to th
 */
 
 class MarsRoverTests extends FlatSpec with Matchers {
+
   "Planet" can "define its size" in {
     val mars = new Planet(new Position(-10,-10), new Position(10,10))
     mars.calculateSize should be (400)
+    val jupiter = new Planet(new Position(-25,-34), new Position(50,40))
+    jupiter.calculateSize should not be (100)
+  }
+
+  "Rover" can "land on planet" in {
+    val RoverOne = new Rover
+    RoverOne.getCurrentPosition.x should be (0)
+    RoverOne.getCurrentPosition.y should be (0)
+    RoverOne.getCurrentDirection() should be ('N')
+  }
+
+  "Rover" can "turn on planet" in {
+    val RoverOne = new Rover
+    RoverOne.getCurrentDirection() should be ('N')
+    RoverOne.instructRover(List('S'))
+    RoverOne.getCurrentDirection() should be ('S')
+  }
+
+  "Rover" can "move on planet" in {
+    val RoverOne = new Rover
+    RoverOne.getCurrentPosition.x should be (0)
+    RoverOne.getCurrentPosition.y should be (0)
+    RoverOne.getCurrentDirection() should be ('N')
+    RoverOne.instructRover(List('M'))
+    RoverOne.getCurrentPosition.x should be (0)
+    RoverOne.getCurrentPosition.y should be (1)
+    RoverOne.getCurrentDirection() should be ('N')
+  }
+
+  "Rover" can "move & turn on planet" in {
+    val RoverOne = new Rover
+    RoverOne.getCurrentPosition.x should be (0)
+    RoverOne.getCurrentPosition.y should be (0)
+    RoverOne.getCurrentDirection() should be ('N')
+    RoverOne.instructRover(List('M','M','E','M','M','M'))
+    RoverOne.getCurrentPosition.x should be (3)
+    RoverOne.getCurrentPosition.y should be (2)
+    RoverOne.getCurrentDirection() should be ('E')
+  }
+
+  "Rover" can "accept and execute orders" in {
+    val RoverOne = new Rover
+    RoverOne.getCurrentPosition.x should be (0)
+    RoverOne.getCurrentPosition.y should be (0)
+    RoverOne.getCurrentDirection() should be ('N')
+    RoverOne.instructRover(List('E','M','M','N','M')) should be (true)
+    RoverOne.getCurrentPosition.x should be (2)
+    RoverOne.getCurrentPosition.y should be (1)
+    RoverOne.getCurrentDirection() should be ('N')
   }
 }
